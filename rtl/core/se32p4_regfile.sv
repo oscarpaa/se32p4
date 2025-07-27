eec4df7323416bfeff97be56ec888d3fe3cf00e4
oscarpaa <oscar.parra.alonso@gmail.com>
2025-07-27 21:01:01 +0200

    Create register file

---
+++ rtl/core/se32p4_regfile.sv
@@ -0,0 +1,33 @@
+
+module se32p4_regfile (
+    input logic clk_i,
+    input logic rst_i,
+    input logic write_e_i,
+    input logic [4:0] read_addr1_i, read_addr2_i, write_addr3_i,
+    output logic [31:0] read_dat1_o, read_dat2_o,
+    input logic [31:0] write_dat3_i
+);
+
+    logic [31:0] REGISTER_MEM [31:0];
+    
+    always_ff @(posedge clk_i, posedge rst_i) begin : write_to_reg
+        if (rst_i) begin
+            for (int i = 0; i < 32; i++) begin
+                REGISTER_MEM[i] <= 32'b0;
+            end
+        end else if (write_e_i) begin
+            if (write_addr3_i != 5'b0) REGISTER_MEM[write_addr3_i] <= write_dat3_i;
+        end
+    end
+    
+    always_ff @(posedge clk_i, posedge rst_i) begin : read_from_reg
+        if (rst_i) begin
+            read_dat1_o <= 32'b0;
+            read_dat2_o <= 32'b0;
+        end else if (!write_e_i) begin
+            read_dat1_o <= (read_addr1_i != 5'b0) ? REGISTER_MEM[read_addr1_i] : 32'b0;
+            read_dat2_o <= (read_addr2_i != 5'b0) ? REGISTER_MEM[read_addr2_i] : 32'b0;
+        end
+    end
+
+endmodule
