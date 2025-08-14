92fb084503af696fd98716a62b0f572cf8d0290c
oscarpaa <oscar.parra.alonso@gmail.com>
2025-08-14 14:55:54 +0200

    Create decoder [unfinished]

--- rtl/core/se32p4_alu.sv
+++ rtl/core/se32p4_alu.sv
@@ -2,7 +2,7 @@
 module se32p4_alu 
     import se32p4_pkg::*;
 (
-    input logic [3:0] alu_oper_i,
+    input aluop_t oper_type_i,
     input logic [31:0] oper_a_i, oper_b_i,
     output logic [31:0] oper_res_o,
     input logic oper_sign_i,
@@ -18,18 +18,18 @@ module se32p4_alu
     assign compare_o = compare;
     
     always_comb begin
-        unique case (alu_oper_i)
-            ALUOP_ADD:  oper_res_o = oper_a_i + oper_b_i;
-            ALUOP_SUB:  oper_res_o = oper_a_i - oper_b_i;
-            ALUOP_AND:  oper_res_o = oper_a_i & oper_b_i;
-            ALUOP_OR:   oper_res_o = oper_a_i | oper_b_i;
-            ALUOP_XOR:  oper_res_o = oper_a_i ^ oper_b_i;
-            ALUOP_SLT:  oper_res_o = {31'b0, compare[1]};
-            ALUOP_SLTU: oper_res_o = {31'b0, compare[1]};
-            ALUOP_SLL:  oper_res_o = oper_a_i << oper_b_i;
-            ALUOP_SRL:  oper_res_o = oper_a_i >> oper_b_i;
-            ALUOP_SRA:  oper_res_o = oper_a_i >>> oper_b_i;
-            default:    oper_res_o = 32'b0;
+        unique case (oper_type_i)
+            ALUOP_ADD:  oper_res_o <= oper_a_i + oper_b_i;
+            ALUOP_SUB:  oper_res_o <= oper_a_i - oper_b_i;
+            ALUOP_AND:  oper_res_o <= oper_a_i & oper_b_i;
+            ALUOP_OR:   oper_res_o <= oper_a_i | oper_b_i;
+            ALUOP_XOR:  oper_res_o <= oper_a_i ^ oper_b_i;
+            ALUOP_SLT:  oper_res_o <= {31'b0, compare[1]};
+            ALUOP_SLTU: oper_res_o <= {31'b0, compare[1]};
+            ALUOP_SLL:  oper_res_o <= oper_a_i << oper_b_i;
+            ALUOP_SRL:  oper_res_o <= oper_a_i >> oper_b_i;
+            ALUOP_SRA:  oper_res_o <= oper_a_i >>> oper_b_i;
+            default:    oper_res_o <= 32'b0;
         endcase
     end
 
--- rtl/system/sys_memory.sv
+++ rtl/system/sys_memory.sv
@@ -1,20 +1,24 @@
 
 module sys_memory #(
-    parameter int MEM_BYTES_LEN = 4096,
-    parameter int ADDR_WIDTH = $clog2(MEM_BYTES_LEN)
+    parameter int MEM_BYTES_LEN = 4096
 ) (
     input logic clk_i,
     input logic rst_i,
     input logic write_e_i,
     input logic [3:0] byte_e_i,
-    input logic [ADDR_WIDTH-1:0] addr_i,
+    input logic [31:0] read_addr1_i,
+    input logic [31:0] write_addr2_i,
     input logic [31:0] write_dat_i,
-    output logic [31:0] read_dat_o
+    output logic [31:0] read_dat1_o,
+    output logic [31:0] read_dat2_o
+
 );
+    localparam int ADDR_WIDTH = $clog2(MEM_BYTES_LEN);
     localparam int MEM_HALF_WORDS_LEN = MEM_BYTES_LEN/2;
     
     logic [15:0] SYS_MEMORY [0:MEM_HALF_WORDS_LEN-1];
-    logic addr = addr_i[ADDR_WIDTH-1:1];
+    logic addr1 = read_addr1_i[ADDR_WIDTH-1:1];
+    logic addr2 = write_addr2_i[ADDR_WIDTH-1:1];
     
     always_ff @(posedge clk_i, posedge rst_i) begin : write_to_sys_mem
         if (rst_i == 1'b1) begin
@@ -23,30 +27,42 @@ module sys_memory #(
             end
         end else if (write_e_i == 1'b1) begin
             if (byte_e_i[0] == 1'b1) begin
-                SYS_MEMORY[addr][7:0] <= write_dat_i[7:0];
+                SYS_MEMORY[addr2][7:0] <= write_dat_i[7:0];
             end
             if (byte_e_i[1] == 1'b1) begin
-                SYS_MEMORY[addr][15:8] <= write_dat_i[15:8];
+                SYS_MEMORY[addr2][15:8] <= write_dat_i[15:8];
             end
-            if ((addr + 1) <= (MEM_HALF_WORDS_LEN - 1)) begin
+            if ((addr2 + 1) <= (MEM_HALF_WORDS_LEN - 1)) begin
                 if (byte_e_i[2] == 1'b1) begin
-                    SYS_MEMORY[addr + 1][7:0] <= write_dat_i[23:16];
+                    SYS_MEMORY[addr2 + 1][7:0] <= write_dat_i[23:16];
                 end           
                 if (byte_e_i[3] == 1'b1) begin
-                    SYS_MEMORY[addr + 1][15:8] <= write_dat_i[31:24];
+                    SYS_MEMORY[addr2 + 1][15:8] <= write_dat_i[31:24];
                 end
             end
         end
     end
     
-    always_ff @(posedge clk_i, posedge rst_i) begin : read_from_sys_mem
-        if (rst_i == 1'b1) read_dat_o <= 32'b0;
+    always_ff @(posedge clk_i, posedge rst_i) begin : read_from_sys_mem_a2
+        if (rst_i == 1'b1) read_dat2_o <= 32'b0;
         else if (write_e_i == 1'b0) begin
-            read_dat_o[15:0] <= SYS_MEMORY[addr];
-            if ((addr + 1) <= (MEM_HALF_WORDS_LEN - 1)) begin
-                read_dat_o[31:16] <= SYS_MEMORY[addr + 1];
+            read_dat2_o[15:0] <= SYS_MEMORY[addr2];
+            if ((addr2 + 1) <= (MEM_HALF_WORDS_LEN - 1)) begin
+                read_dat2_o[31:16] <= SYS_MEMORY[addr2 + 1];
             end else begin
-                read_dat_o[31:16] <= 16'b0;
+                read_dat2_o[31:16] <= 16'b0;
+            end
+        end
+    end
+    
+    always_ff @(posedge clk_i, posedge rst_i) begin : read_from_sys_mem_a1
+        if (rst_i == 1'b1) read_dat1_o <= 32'b0;
+        else if (write_e_i == 1'b0) begin
+            read_dat1_o[15:0] <= SYS_MEMORY[addr1];
+            if ((addr1 + 1) <= (MEM_HALF_WORDS_LEN - 1)) begin
+                read_dat1_o[31:16] <= SYS_MEMORY[addr1 + 1];
+            end else begin
+                read_dat1_o[31:16] <= 16'b0;
             end
         end
     end
